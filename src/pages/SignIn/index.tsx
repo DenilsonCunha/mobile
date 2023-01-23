3e3578835f04e82f8f09d65849d04f3c5f54b48c
DenilsonCunha <denilsonluciano6@gmail.com>
2023-01-23 11:42:35 -0300

    config login e pagina de novos pedidos

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,25 +5,27 @@ import {
   StyleSheet,
   Image,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  ActivityIndicator
 } from 'react-native'
 
 import { AuthContext } from '../../contexts/AuthContext'
 
 export default function SignIn(){
   
-  const { user } = useContext(AuthContext)
+  const { signIn, loadingAuth} = useContext(AuthContext)
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  function handleLogin(){
+  async function handleLogin(){
 
     if(email === '' || password === ''){
       return;
     }
 
-    console.log("Email digitado " + email)
+    await signIn({ email, password })
+    
   }
 
 
@@ -53,7 +55,11 @@ export default function SignIn(){
         />     
 
         <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Acessar</Text>
+          { loadingAuth ? (
+            <ActivityIndicator size={25} color="#FFF"/>
+          ) : (
+            <Text style={styles.buttonText}>Acessar</Text>
+          )}
         </TouchableOpacity>   
       </View>
 
